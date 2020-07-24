95a4139cc1434099f3910bacc108bea9c29a1cdf
Alex Foster <alfozi@Alex-Fosters-MacBook-Pro-Retina.local>
2020-07-24 12:31:17 +0200

    first commit

---
+++ client/modalScript.js
@@ -0,0 +1,9 @@
+let modal = document.getElementById("myModal");
+const nicknameInput = document.getElementById("nickname-input");
+
+nicknameInput.onkeypress = e => {
+    let keycode = (e.keyCode ? e.keyCode : e.which);
+    if(keycode == "13"){
+        modal.style.display = "none";
+    }
+};
